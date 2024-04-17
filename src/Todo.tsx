dabed5975ae4e1efc34794bb9a8d2b6f57c0ff83
migvidal <mvidal@air-institute.com>
2024-04-17 21:06:41 +0200

    Change opacity on checked + less stupid dummy text

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -10,7 +10,7 @@ export class Todo {
 }
 
 export const todos = [
-  new Todo("Buy bread", "Whole grain from the cerel aisle", false),
+  new Todo("Buy bread", "Whole grain from the cereal aisle", false),
   new Todo("Buy cereal", "Corn flakes, Kellogs brand", false),
-  new Todo("Do your mom", "Got em", true),
+  new Todo("Get Amazon package", "From the locker @ the gas station", true),
 ]
--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 export const TodoItem = ({ title, description, isImportant, onClick, isChecked }: { title: string, description: string, isImportant: boolean, onClick: () => any, isChecked: boolean }) => {
-    let checkedStyle = isChecked ? "line-through" : ""
+    let checkedStyle = isChecked ? "line-through opacity-70" : ""
     let commonStyle = checkedStyle + " rounded-lg my-4 p-4 border-2 cursor-pointer "
     let regularStyle = commonStyle + "bg-cyan-200 border-cyan-400 dark:bg-cyan-800 dark:border-cyan-600"
     let importantStyle = commonStyle + "bg-red-200 border-red-400 dark:bg-red-800 dark:border-red-600"
